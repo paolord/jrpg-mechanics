d439053963bbe52f9183fa50dfa28b67b3b99871
angelo <angelopaolo.obispo@gmail.com>
2021-01-02 15:13:40 +0800

    commit before branching off to ATB tutorial

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    [SerializeField] private Vector3Int position;
+
+    private void Awake()
+    {
+        if (position == null)
+        {
+            position = new Vector3Int(0, 0, 0);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# JRPG Mechanics
+
+Personal Unity/C# practice exercise.
+
+Quality is a goal but it's not guaranteed. So, *do not use* any code in another project.
+
+Pixel Art resources credited to Antifarea and https://opengameart.org
