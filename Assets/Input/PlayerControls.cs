7731314cc4544cd88d770587b34ca997675dd638
angelo <angelopaolo.obispo@gmail.com>
2021-01-03 00:36:18 +0800

    Overworld Stage - Precision Directional Control

---
+++ Assets/Input/PlayerControls.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""1dac8f61-6663-4524-abee-51439fdedc38"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Button"",
+                    ""id"": ""9b9df353-bc1c-4fd2-a2fd-af409cb0b0d3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""68e76751-9659-4669-b80f-4c4403be9b54"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""dda0e333-7ef0-4fe2-b389-b8edba8a3872"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""70f580df-0782-4223-bec0-2548b35cc286"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""de893233-8a05-41a4-a580-508141414c21"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""654c27f6-cdd2-4895-b681-e6817202bdf8"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    public struct PlayerActions
+    {
+        private @PlayerControls m_Wrapper;
+        public PlayerActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/OverworldController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OverworldController : MonoBehaviour
+{
+    [SerializeField] private Tilemap _field;
+    [SerializeField] private GameObject _playerObject;
+    [SerializeField] private float _movementSpeed;
+
+    private PlayerControls _playerControls;
+    private Animator _playerMovementAnimator;
+    private SpriteRenderer _playerSpriteRenderer;
+    private Vector3 _nextPosition;
+    private Vector2 _previousDirection;
+    private bool _moving;
+    [SerializeField] private float _inputDelay;
+    private float _inputDelayCurrent;
+    private bool _waiting;
+
+    private void Awake()
+    {
+        _field.CompressBounds();
+
+        _playerControls = new PlayerControls();
+        
+        _playerMovementAnimator = _playerObject.GetComponent<Animator>();
+        _playerSpriteRenderer = _playerObject.GetComponent<SpriteRenderer>();
+
+        Vector3 pos = _field.GetCellCenterWorld(new Vector3Int(1, 2, 0));
+
+        // Set Starting Position
+        _playerObject.transform.position = pos;
+        _moving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementInput = _playerControls.Player.Move.ReadValue<Vector2>();
+        
+        if (_playerObject.transform.position == _nextPosition)
+        {
+            _moving = false;
+
+            //_playerMovementAnimator.Play("Movement", 0, 0.0f);
+            _playerMovementAnimator.SetBool("Moving", false);
+        }
+        updateDelay();
+        if ((movementInput.x != 0 || movementInput.y != 0) && !_waiting)
+        {
+            if (!_moving)
+            {
+                _playerSpriteRenderer.flipX = movementInput.x < 0;
+
+                _playerMovementAnimator.SetBool("Idle", false);
+
+                if (_previousDirection == movementInput)
+                {
+                    _moving = true;
+                    _playerMovementAnimator.SetBool("Moving", true);
+                }
+                else
+                {
+                    _inputDelayCurrent = _inputDelay;
+                    _waiting = true;
+                    _playerMovementAnimator.SetFloat("Horizontal", movementInput.x);
+                    _playerMovementAnimator.SetFloat("Vertical", movementInput.y);
+                }
+
+                Vector3Int nextCellLocation = getNextMoveTile(movementInput);
+                _nextPosition = _field.GetCellCenterWorld(nextCellLocation);
+
+                TileBase currentTile = _field.GetTile(nextCellLocation);
+                if (currentTile == null || currentTile.name == "tilemap_2")
+                {
+                    _moving = false;
+                    _playerMovementAnimator.SetBool("Moving", false);
+                }
+                _previousDirection = movementInput;
+            }
+        }
+    }
+    private Vector3Int getNextMoveTile(Vector2 movementVector)
+    {
+        Vector3Int playerCellLocation = _field.WorldToCell(_playerObject.transform.position);
+
+        playerCellLocation.x += (int)movementVector.x;
+        playerCellLocation.y += (int)movementVector.y;
+
+        return playerCellLocation;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_moving)
+        {
+            _playerObject.transform.position = Vector3.MoveTowards(
+                _playerObject.transform.position,
+                _nextPosition,
+                _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void updateDelay()
+    {
+        if (_waiting && _inputDelayCurrent > 0)
+        {
+            _inputDelayCurrent -= Time.deltaTime;
+        }
+        if (_inputDelayCurrent < 0)
+        {
+            _inputDelayCurrent = 0;
+            _waiting = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+}
